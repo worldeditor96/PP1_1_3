5da411dbaa75059ae1365a3949c9af3e1469c48f
Maksim Penza <w_max_hot_e@mail.ru>
2021-12-03 17:34:47 +0300

    03.12.2021

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -15,161 +15,56 @@ public class UserDaoHibernateImpl implements UserDao {
     }
 
     public void createUsersTable() {
-
-        Transaction transaction = null;
-        Session session = null;
-
-        try {
-            session = Util.getSessionFactory().openSession();
-            transaction = session.beginTransaction();
-
-            session.createSQLQuery("CREATE TABLE IF NOT EXISTS User (id MEDIUMINT NOT NULL AUTO_INCREMENT, name VARCHAR (30) NOT NULL, lastName VARCHAR (30) NOT NULL, age MEDIUMINT, PRIMARY KEY (id));").executeUpdate();
-
-            transaction.commit();
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            e.printStackTrace();
-        } finally {
-            if (session != null) {
-                session.close();
-            }
-        }
+        Session session = Util.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        session.createSQLQuery("CREATE TABLE IF NOT EXISTS User (id MEDIUMINT NOT NULL AUTO_INCREMENT, name VARCHAR (30) NOT NULL, lastName VARCHAR (30) NOT NULL, age MEDIUMINT, PRIMARY KEY (id));").executeUpdate();
+        transaction.commit();
+        session.close();
     }
 
     public void dropUsersTable() {
-
-        Transaction transaction = null;
-        Session session = null;
-
-        try {
-            session = Util.getSessionFactory().openSession();
-            transaction = session.beginTransaction();
-            session.createSQLQuery("DROP TABLE IF EXISTS user").executeUpdate();
-            transaction.commit();
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            e.printStackTrace();
-        } finally {
-            if (session != null) {
-                session.close();
-            }
-        }
+        Session session = Util.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        session.createSQLQuery("DROP TABLE IF EXISTS user").executeUpdate();
+        transaction.commit();
+        session.close();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        Transaction transaction = null;
-        Session session = null;
-
-        try {
-            session = Util.getSessionFactory().openSession();
-            transaction = session.beginTransaction();
-            User user = new User(name, lastName, age);
-            session.save(user);
-            transaction.commit();
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            e.printStackTrace();
-        } finally {
-            if (session != null) {
-                session.close();
-            }
-        }
+        Session session = Util.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        User user = new User(name, lastName, age);
+        session.save(user);
+        transaction.commit();
+        session.close();
     }
 
     public void removeUserById(long id) {
-        Transaction transaction = null;
-        Session session = null;
-
-        try {
-            session = Util.getSessionFactory().openSession();
-            transaction = session.beginTransaction();
-
-            session.createSQLQuery("DElETE FROM User WHERE id=?").setLong(1, id).executeUpdate();
-
-            //Рабочий код Вариант 1
-            //User user = session.load(User.class, id);
-            //session.delete(user);
-
-            /*//Рабочий код Вариант 2
-            preparedStatement = connection.prepareStatement("DElETE FROM User WHERE id=?");
-            preparedStatement.setLong(1, id);
-            preparedStatement.executeUpdate();
-
-             */
-
-            //Рабочий код Вариант 3
-            //session.delete(session.get(User.class, id));
-
-            transaction.commit();
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            e.printStackTrace();
-        } finally {
-            if (session != null) {
-                session.close();
-            }
-        }
+        Session session = Util.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        session.createSQLQuery("DElETE FROM User WHERE id=?").setLong(1, id).executeUpdate();
+        transaction.commit();
+        session.close();
     }
 
 
     public List<User> getAllUsers() {
         List<User> users = new ArrayList<>();
-        Transaction transaction = null;
-        Session session = null;
-
-        try {
-            session = Util.getSessionFactory().openSession();
-            transaction = session.beginTransaction();
-
-            // Пробую разные варианты
-            //users =session.createSQLQuery("FROM User").list();
-
-            Query query = session.createQuery("FROM User");
-            users = query.list();
-
-
-            transaction.commit();
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            e.printStackTrace();
-        } finally {
-            if (session != null) {
-                session.close();
-            }
-        }
+        Session session = Util.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        Query query = session.createQuery("FROM User");
+        users = query.list();
+        transaction.commit();
+        session.close();
         return users;
     }
 
     public void cleanUsersTable() {
-        Transaction transaction = null;
-        Session session = null;
-
-        try {
-            session = Util.getSessionFactory().openSession();
-            transaction = session.beginTransaction();
-
-            session.createSQLQuery("TRUNCATE TABLE user").executeUpdate();
-
-            transaction.commit();
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            e.printStackTrace();
-        } finally {
-            if (session != null) {
-                session.close();
-            }
-        }
+        Session session = Util.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        session.createSQLQuery("TRUNCATE TABLE user").executeUpdate();
+        transaction.commit();
+        session.close();
     }
+
 }
